d7a67c3afd75a9cb4c4969bd9facf2fc12984749
simondoesstuff <sw.simonwalker@gmail.com>
2022-11-26 05:00:11 -0700

    partially made test for index JS

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,12 @@
-const axios = require('axios');
-const tdbApi = require('../resources/js/tdbApi.js');
-
-test('does /quiz give a valid quiz?', async () => {
-    const category = tdbApi.categories[Math.random() * tdbApi.categories.length];
-    const difficulty = tdbApi.difficulties[Math.random() * tdbApi.difficulties.length];
-
-    const resp = await axios.get(`/quiz?category=${category}&difficulty=${difficulty}`);
-    console.log(resp.data);
-})
+// const axios = require('axios');
+// const tdbApi = require('../resources/js/tdbApi.js');
+//
+// const host = '172.17.0.1:3000';
+//
+// test('does /quiz give a valid quiz?', async () => {
+//     const category = tdbApi.categories[Math.random() * tdbApi.categories.length];
+//     const difficulty = tdbApi.difficulties[Math.random() * tdbApi.difficulties.length];
+//
+//     const resp = await axios.get(`${host}/quiz?category=${category}&difficulty=${difficulty}`);
+//     console.log(resp.data);
+// })
